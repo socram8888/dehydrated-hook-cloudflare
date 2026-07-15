de85ea161b5926db9cce06b4ba63db37e356872a
Mehul Sanghvi <mehul.sanghvi@gmail.com>
2026-07-15 02:29:39 -0400

    Remove extra repetition operator in sed command
    When running on FreeBSD, using `/usr/bin/sed`, the repetition sequence
    of `.*?` generates the following error:
    
    ```
    sed: 1: "s/^.*?\.\t[0-9]+\tIN\tT ...": RE error: repetition-operator operand invalid
    ```
    
    This is because the regex has two repetition operators back to back
    and there are no valid operand between them.  The `*` applies to `.`
    and then `?` tries to apply a repetition to the result of that
    repetition, whcih the regex library rejects in FreeBSD's extended
    mode (-r/-E).  GNU sed's regex library is somewhat more lenient and
    just lets it slide.
    
    The solution is to drop the `?` as it is not doing anything useful
    anyway.  The `?` after the `.*` doesn't create non-greedy matching in
    extended regex mode, so removing it doesn't change the intended
    behaviour.

--- cf-hook.sh
+++ cf-hook.sh
@@ -26,7 +26,7 @@ fi
 
 if which drill &>/dev/null; then
 	resolve_record() {
-		drill "$1" "$2" @ns.cloudflare.com | sed -rn "s/^.*?\.\t[0-9]+\tIN\t$2\t//p"
+		drill "$1" "$2" @ns.cloudflare.com | sed -rn "s/^.*\.\t[0-9]+\tIN\t$2\t//p"
 	}
 elif which dig &>/dev/null; then
 	resolve_record() {
